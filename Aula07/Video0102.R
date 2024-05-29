058da7d3bceafc2fd0c64addb3f90321c0f8240f
alemnds <alexandre.mendes1980@outlook.com>
2024-05-29 01:28:27 +0000

    Video02 - Finalizado

--- Aula07/Video0102.R
+++ Aula07/Video0102.R
@@ -17,3 +17,23 @@ perfil
 wilcox.test(gasto~perfil)
 length(gasto)
 length(perfil)
+
+perfil
+
+pre<- scan()
+pos<- scan()
+
+wilcox.test(pre,pos,paired = T)
+
+
+# identificou que 39 pessoas de uma amostra de 215 tem uma caracteristica. 
+
+prop.test(39,215,.15)
+
+# consumo de cafeina e estado civil 
+
+cafe.estado<- matrix(c(652,1537,598,242,36,46,38,21,218,327,106,67),nrow = 3,byrow = T)
+rownames(cafe.estado)<- c("casado", "divorciado","solteiro")
+colnames(cafe.estado)<- c("0","1-150","151-300",">300")
+
+chisq.test(cafe.estado)
